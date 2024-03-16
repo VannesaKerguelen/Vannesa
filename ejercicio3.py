85d7ece5f93d538d47d34721c4fc1c514026d4fe
VannesaKerguelen <163611658+VannesaKerguelen@users.noreply.github.com>
2024-03-15 20:32:56 -0500

    Add files via upload

---
+++ ejercicio3.py
@@ -0,0 +1,121 @@
+"""print ("calcular el area de un triangulo") 
+
+base=float(input("digite el valor de la base")) 
+
+altura=float(input("digite el valor de la altura")) 
+
+  
+
+resultado=(base*altura)/2 
+
+  
+
+print("el resultado es"+ str (resultado)) 
+
+  
+
+print ("digite dos numeros y sumar") 
+
+primer_digito=float(input("digite el valor")) 
+
+segundo_digito=float(input("digite el valor")) 
+
+  
+
+resultado=(primer_digito+segundo_digito) 
+
+  
+
+print("el resultado es"+ str (resultado)) 
+
+  
+
+  
+
+  
+
+  
+
+  
+
+  
+
+print ("ingrese un digito para asi elevarlo al cuadrado") 
+
+digito=float(input("ingrese el valor")) 
+
+  
+
+resultado=digito*digito 
+
+  
+
+print ("el resultado es"+ str(resultado)) 
+
+  
+
+print("convierta euros a dolares") 
+
+euros=float(input("digite la cantidad de euros")) 
+
+dolares=1.08 
+
+  
+
+conversion=(euros*dolares) 
+
+  
+
+  
+
+print("el resultado del perimetro es"+ str(conversion)) 
+
+  
+
+print ("ingrese el valor del lado de un cuadrado para asi hallar  el perimetro") 
+
+  
+
+digito=float(input("ingrese el valor")) 
+
+  
+
+  
+
+resultado=digito+digito+digito+digito 
+
+  
+
+print ("el resultado del perimetro es"+ str(resultado)) 
+
+  
+
+print ("ingrese el valor del lado de un cuadrado para asi hallar  el area") 
+
+  
+
+digito=float(input("ingrese el valor")) 
+
+resultado=digito*digito 
+
+print ("el resultado del area es"+ str(resultado)) 
+
+""" 
+
+  
+
+print ("cilindro para asi hallar  el volumen") 
+
+  
+
+radio=float(input("ingrese el valor del radio")) 
+
+altura=float(input("ingrese el valor altura")) 
+
+pi= 3.14 
+
+resultado= pi* radio*radio* altura 
+
+print ("el resultado del volumen es"+ str(resultado))   
+
+ 
